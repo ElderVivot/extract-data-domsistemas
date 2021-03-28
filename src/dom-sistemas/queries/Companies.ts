13de2444f7e77637b9283d10c5e205f2a9dafabf
ElderVivot <eldervivot@gmail.com>
2021-03-28 17:06:22 -0300

    chore: update script create_service to aeron, delete .env, delete sql of companie and replace to sql in Companies.ts

--- src/dom-sistemas/queries/Companies.ts
+++ src/dom-sistemas/queries/Companies.ts
@@ -1,6 +1,4 @@
-import fs from 'fs'
 import { Connection } from 'odbc'
-import path from 'path'
 
 import DB from '../db'
 
@@ -11,7 +9,28 @@ export default class Companies {
 
     constructor () {
         this.db = new DB()
-        this.sql = fs.readFileSync(path.resolve(__dirname, './sql/companies.sql')).toString()
+        this.sql = `
+            SELECT  emp.codi_emp AS code,
+                    emp.nome_emp AS name,
+                    emp.apel_emp AS nickName,
+                    emp.tins_emp AS typeCgce,
+                    COALESCE(TRIM(emp.cgce_emp), '') AS cgce,
+                    emp.stat_emp AS status,
+                    emp.dddf_emp AS ddd,
+                    emp.fone_emp AS fone,
+                    emp.email_emp AS email,
+                    emp.ramo_emp AS ramo,
+                    emp.dtinicio_emp AS dateInicialAsCompanie,
+                    emp.dcad_emp AS dateInicialAsClient,
+                    emp.dina_emp AS dateFinalAsClient,
+                    emp.iest_emp AS inscricaoEstadual,
+                    emp.imun_emp AS inscricaoMunicipal,
+                    emp.esta_emp AS uf
+            
+               FROM bethadba.geempre AS emp
+            
+            ORDER BY emp.codi_emp
+        `
     }
 
     async export (): Promise<any> {
